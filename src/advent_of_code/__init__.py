bd18e32f5f5d764c22c29ded2e67fab0f22516ae
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-21 13:43:00 -0400

    update module name

--- src/advent_of_code/__init__.py
+++ src/advent_of_code/__init__.py
@@ -1,2 +1,2 @@
 from .constants import DATA_DIR
-from .apk import get_example, get_input, print_description, get_description
+from .sdk import get_example, get_input, print_description, get_description
