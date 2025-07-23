a32a52c839954b477f879396db02f8faa944c04f
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-23 09:34:09 -0400

    solved 2015-8 and 2015-9!

---
+++ solutions/2015/day8.py
@@ -0,0 +1,118 @@
+import ast
+import codecs
+import functools
+import itertools
+import json
+import math
+import os
+import re
+from copy import deepcopy
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum
+from pathlib import Path
+from pprint import pprint
+from string import ascii_letters
+from typing import Callable, NamedTuple, Optional, Protocol, Self
+
+import numpy as np
+import pandas as pd
+import polars as pl
+from alive_progress import alive_it
+from rich import print
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day'))
+
+CURRENT_DIR = Path.home() / 'python' / 'advent_of_code' / 'solutions' / '2015'
+
+INPUT = aoc.get_input(YEAR, DAY)
+DESCRIPTION = aoc.get_description(YEAR, DAY)
+
+def part_one_example():
+    with open(CURRENT_DIR / 'day8_examples.txt', 'r') as f:
+        total_code_len = 0
+        total_string_len = 0
+        for s in f.readlines():
+            s = s.strip('\n')
+            code_len = len(s)
+            total_code_len += code_len
+            string_len = len(s
+                             .removeprefix('\"')
+                             .removesuffix('\"')
+                             .encode('utf-8')
+                             .decode('unicode_escape'))
+            total_string_len += string_len
+        return total_code_len - total_string_len
+
+def part_two_example():
+    total_code_len = 0
+    total_escape_len = 0
+    with open(CURRENT_DIR / 'day8_examples.txt', 'r') as f:
+        for s in f.readlines():
+            s = s.strip('\n')
+            code_len = len(s)
+            total_code_len += code_len
+            
+            s1 = re.escape(s)
+            s2 = re.sub(r'([\",])', r'\\\1', s1)
+            escape_len = len(s2) + 2  # adding two for the outer quotation marks
+            total_escape_len += escape_len
+        return total_escape_len - total_code_len
+
+
+def parse_data(data: str):
+    line_list = [line for line in data.split('\n') if line]
+    return line_list
+    
+def part_one():
+    with open(CURRENT_DIR / 'day8_input.txt', 'r') as f:
+        total_code_len = 0
+        total_string_len = 0
+        for s in f.readlines():
+            s = s.strip('\n')
+            code_len = len(s)
+            total_code_len += code_len
+            string_len = len(s
+                             .removeprefix('\"')
+                             .removesuffix('\"')
+                             .encode('utf-8')
+                             .decode('unicode_escape'))
+            total_string_len += string_len
+        return total_code_len - total_string_len
+
+def part_two():
+    total_code_len = 0
+    total_escape_len = 0
+    with open(CURRENT_DIR / 'day8_input.txt', 'r') as f:
+        for s in f.readlines():
+            s = s.strip('\n')
+            code_len = len(s)
+            total_code_len += code_len
+            
+            s1 = re.escape(s)
+            s2 = re.sub(r'([\",])', r'\\\1', s1)
+            escape_len = len(s2) + 2  # adding two for the outer quotation marks
+            total_escape_len += escape_len
+        return total_escape_len - total_code_len
+
+
+
+
+def main():
+    print(f"Part One (example): {part_one_example()}")
+    print(f"Part One (input):  {part_one()}")
+    print()
+    print(f"Part Two (example):  {part_two_example()}")
+    print(f"Part Two (input):  {part_two()}")
+
+    random_tests()
+
+def random_tests():
+    ...
+
+       
+if __name__ == '__main__':
+    main()
---
+++ solutions/2015/day9.py
@@ -0,0 +1,103 @@
+import functools
+import itertools
+import json
+import math
+import os
+from copy import deepcopy
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum
+from pathlib import Path
+from pprint import pprint
+from string import ascii_letters
+from typing import Callable, NamedTuple, Optional, Protocol, Self
+
+import numpy as np
+import pandas as pd
+import polars as pl
+from alive_progress import alive_it
+from rich import print
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day'))
+
+EXAMPLE = aoc.get_example(YEAR, DAY)
+INPUT = aoc.get_input(YEAR, DAY)
+DESCRIPTION = aoc.get_description(YEAR, DAY)
+
+@dataclass
+class Flight:
+    start: str
+    end: str
+    distance: int
+
+@dataclass
+class FlightSet:
+    flights: list[Flight]
+
+    def __post_init__(self):
+        self.all_cities = ({f.start for f in self.flights} 
+                           | {f.end for f in self.flights})
+
+    def get_distance(self, start: str, end: str) -> int:
+        flight = [f for f in self.flights if 
+                  (f.start == start and f.end == end)
+                  or (f.start == end and f.end == start)][0]
+        return flight.distance
+
+    def get_route(self, distance: int) -> Flight:
+        return [f for f in self.flights if f.distance == distance][0]
+
+def parse_data(data: str) -> FlightSet:
+    line_list = [line for line in data.split('\n') if line]
+    output_list = []
+    for line in line_list:
+        parts = line.split(' ')
+        output_list.append(Flight(parts[0], parts[2], int(parts[4])))
+    return FlightSet(output_list)
+
+def get_route_distances(flight_set: FlightSet) -> list[int]:
+    perms = itertools.permutations(flight_set.all_cities, 
+                                   len(flight_set.all_cities))
+
+    route_distances = []
+    for route in perms:
+        route_distance = 0
+        for i, city in enumerate(route):
+            if i == len(route) - 1:
+                break
+            route_distance += flight_set.get_distance(city, route[i+1])
+        route_distances.append(route_distance)
+    return route_distances
+
+    
+def part_one(data: str):
+    flight_set = parse_data(data)
+    route_distances = get_route_distances(flight_set)
+    return min(route_distances)       
+    
+
+def part_two(data: str):
+    flight_set = parse_data(data)
+    route_distances = get_route_distances(flight_set)
+    return max(route_distances)
+
+
+
+def main():
+    print(f"Part One (example):  {part_one(EXAMPLE)}")
+    print(f"Part One (input):  {part_one(INPUT)}")
+    print()
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
+
+    random_tests()
+
+def random_tests():
+    ...
+
+       
+if __name__ == '__main__':
+    main()
