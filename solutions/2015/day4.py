0fb80a8750ad2485cd469ff6beb9f81ff79c6778
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-23 22:26:02 -0400

    clean up

--- solutions/2015/day4.py
+++ solutions/2015/day4.py
@@ -17,8 +17,6 @@ import pandas as pd
 import polars as pl
 from alive_progress import alive_it
 from rich import print
-from rich.live import Live
-from rich.progress import Progress
 
 import advent_of_code as aoc
 
@@ -33,19 +31,6 @@ DESCRIPTION = aoc.get_description(YEAR, DAY)
 
 def get_hash(s: str):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
-    
-# def part_one(secret_key: str) -> int:
-#     with Progress() as progress:
-#         task = progress.add_task("Searching...", total=None)
-#         test_num = 0
-#         while True:
-#             # print(test_num)
-#             if get_hash(f"{secret_key}{test_num}")[0:5] == '00000':
-#                 return test_num
-#             else:
-#                 test_num += 1
-#             if test_num % 1000 == 0:
-#                 progress.update(task)
 
 def parse_data(data: str) -> str:
     return data.strip('\n')
@@ -75,18 +60,15 @@ def main():
         print(f"Part One Example {example}: {part_one(example)}")
     print()
     print(f"Part One Input:  {part_one(INPUT)}")
-    # print()
     for example in EXAMPLES:
         print(f"Part Two Example {example}: {part_two(example)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
 
-    # random_tests()
+    random_tests()
 
 
 def random_tests():
-    # print(get_hash(str('abcdef609043'))[0:5])
-    # print(get_hash(str('abcdef609043')))
-    print(get_hash('ckczppom490464')[0:5])
+    ...
     
 
        
