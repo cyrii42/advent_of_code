d94a9c54c23ea58102aa5e8a60bd1ac38795cfc9
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-25 13:04:19 -0400

    solved 2015 day 12 (part one)

--- solutions/2015/day12.py
+++ solutions/2015/day12.py
@@ -16,6 +16,8 @@ import pandas as pd
 import polars as pl
 from alive_progress import alive_it
 from rich import print
+from rich.pretty import Pretty
+from rich.table import Table
 
 import advent_of_code as aoc
 
@@ -29,14 +31,48 @@ DESCRIPTION = aoc.get_description(YEAR, DAY)
 def parse_data(data: str):
     return data.strip('\n')
 
+def prettify(*args):
+    arg_list = list(args)
+    return [Pretty(arg) for arg in arg_list]
+
+def test_examples() -> None:
+    examples = {
+        '[1,2,3]': 6,
+        '{"a":2,"b":4}': 6,
+        '[[[3]]]': 3,
+        '{"a":{"b":4},"c":-1}': 3,
+        '{"a":[-1,1]}': 0,
+        '[-1,{"a":1}]': 0,
+        '[]': 0,
+        '{}': 0
+    }
+
+    table = Table()
+    table.add_column('Example', justify='left')
+    table.add_column('Expected', justify='center')
+    table.add_column('Actual', justify='center')
+    table.add_column('Passed?', justify='center')
+    for example, expected in examples.items():
+        print(f"Testing {example} ({type(example)})...")
+        result = part_one(example)
+        row_items = prettify(json.loads(example), expected, result, result == expected)
+        table.add_row(*row_items)
+    print(table)
     
 def part_one(data: str):
     s = parse_data(data)
-    d: dict = json.loads(s)
-    return traverse_json(d)
+    obj = json.loads(s)
+    return traverse_json(obj)
 
-def traverse_json(d):
-    ...
+def traverse_json(obj: dict | list | str | int):
+    output = 0
+    if isinstance(obj, int):
+        output += obj
+    if isinstance(obj, list):
+        output += sum(traverse_json(x) for x in obj)
+    if isinstance(obj, dict):
+        output += sum(traverse_json(v) for v in obj.values())
+    return output
         
 
 def part_two(data: str):
@@ -45,9 +81,10 @@ def part_two(data: str):
 
 
 def main():
+    test_examples()
     print(f"Part One (input):  {part_one(INPUT)}")
-    print()
-    print(f"Part Two (input):  {part_two(INPUT)}")
+    # print()
+    # print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
