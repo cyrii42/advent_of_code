9d81ac3386a2ca6397aad989e10eb1d15779741f
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-25 16:43:31 -0400

    solved 2015 day 15 part #1; working on part #2...

--- solutions/2015/day15.py
+++ solutions/2015/day15.py
@@ -2,6 +2,7 @@ import functools
 import itertools
 import json
 import math
+import operator
 import os
 from copy import deepcopy
 from dataclasses import dataclass, field
@@ -28,7 +29,18 @@ EXAMPLE = aoc.get_example(YEAR, DAY)
 INPUT = aoc.get_input(YEAR, DAY)
 DESCRIPTION = aoc.get_description(YEAR, DAY)
 
-class Ingredient(NamedTuple):
+class CookieMismatch(Exception):
+    pass
+
+class Characteristic(Enum):
+    CAPACITY = 0
+    DURABILITY = 1
+    FLAVOR = 2
+    TEXTURE = 3
+    CALORIES = 4
+
+@dataclass
+class Ingredient():
     name: str
     capacity: int
     durability: int
@@ -36,19 +48,52 @@ class Ingredient(NamedTuple):
     texture: int
     calories: int
 
+    def get_score(self, characteristic: Characteristic, tsp: int) -> int:
+        match characteristic:
+            case Characteristic.CAPACITY:
+                return tsp * self.capacity
+            case Characteristic.DURABILITY:
+                return tsp * self.durability
+            case Characteristic.FLAVOR:
+                return tsp * self.flavor
+            case Characteristic.TEXTURE:
+                return tsp * self.texture
+            case Characteristic.CALORIES:
+                return tsp * self.calories
 
 @dataclass
 class Cookie:
     ingredients: list[Ingredient]
+    amounts: tuple[int, ...]
+    total_score: int = 0
+    total_capacity: int = 0
+    total_durability: int = 0
+    total_flavor: int = 0
+    total_texture: int = 0
+    total_calories: int = 0
 
     def __post_init__(self):
-        self.num_ingredients = len(self.ingredients)
-        self.combos = (x for x in 
-                       itertools.permutations([x for x in range(101)], self.num_ingredients) 
-                       if sum(x) == 100)
+        if len(self.amounts) != len(self.ingredients):
+            raise CookieMismatch(f"Invalid combo length: {len(self.amounts)}")
+        
+        for i, ingredient in enumerate(self.ingredients):
+            tsp = self.amounts[i]
+            self.total_capacity += ingredient.get_score(Characteristic.CAPACITY, tsp)
+            self.total_durability += ingredient.get_score(Characteristic.DURABILITY, tsp)
+            self.total_flavor += ingredient.get_score(Characteristic.FLAVOR, tsp)
+            self.total_texture += ingredient.get_score(Characteristic.TEXTURE, tsp)
+            self.total_calories += ingredient.get_score(Characteristic.CALORIES, tsp)
+            
+        self.total_score = self.get_total_score()
 
-    def test_combo(self):
-        ...
+    def get_total_score(self) -> int:
+        raw_nums = [self.total_capacity, self.total_durability, self.total_flavor, self.total_texture]
+        nums_to_multiply = [max(num, 0) for num in raw_nums]
+
+        return functools.reduce(operator.mul, nums_to_multiply, 1)
+
+        
+    
 
 def parse_data(data: str):
     line_list = data.splitlines()
@@ -56,7 +101,7 @@ def parse_data(data: str):
     output_list = []
     for line in line_list:
         parts = line.split(' ')
-        name = parts[0]
+        name = parts[0].strip(':')
         capacity = int(parts[2].strip(','))
         durability = int(parts[4].strip(','))
         flavor = int(parts[6].strip(','))
@@ -65,23 +110,35 @@ def parse_data(data: str):
             
         output_list.append(Ingredient(name, capacity, durability, flavor, texture, calories))
                 
-    return Cookie(output_list)
+    return output_list
     
 def part_one(data: str):
-    cookie = parse_data(data)
-    print(cookie)
+    ingredient_list = parse_data(data)
+    combos = (x for x in 
+              itertools.permutations([x for x in range(101)], len(ingredient_list)) 
+              if sum(x) == 100)
 
+    return max(Cookie(ingredient_list, combo).total_score for combo in combos)
+
+        
 def part_two(data: str):
-    __ = parse_data(data)
+    ingredient_list = parse_data(data)
+    combos = (x for x in 
+              itertools.permutations([x for x in range(101)], len(ingredient_list)) 
+              if sum(x) == 100)
+
+    cookies = (Cookie(ingredient_list, combo) for combo in combos)
+
+    return max(cookie.total_score for cookie in cookies if cookie.total_calories == 500)
 
 
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    # print(f"Part Two (input):  {part_two(INPUT)}")
+    print()
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
