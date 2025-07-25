cd52dadacf06530dbc42a3f61e444b7d831b1f03
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-25 18:36:19 -0400

    started 2015 day #17...

---
+++ solutions/2015/day17.py
@@ -0,0 +1,58 @@
+import functools
+import itertools
+import json
+import math
+import os
+from copy import deepcopy
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum
+from pathlib import Path
+from pprint import pprint
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+from typing import Callable, NamedTuple, Optional, Protocol, Self
+
+import numpy as np
+import pandas as pd
+import polars as pl
+from alive_progress import alive_it
+from rich import print
+from rich.table import Table
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day'))
+
+EXAMPLE = '20\n15\n10\n5\n5'
+INPUT = aoc.get_input(YEAR, DAY)
+DESCRIPTION = aoc.get_description(YEAR, DAY)
+
+def parse_data(data: str) -> list[int]:
+    line_list = data.splitlines()
+    return [int(x) for x in line_list]
+    
+def part_one(data: str, target: int):
+    container_sizes = parse_data(data)
+
+
+def part_two(data: str):
+    __ = parse_data(data)
+
+
+
+def main():
+    print(f"Part One (example):  {part_one(EXAMPLE, target=25)}")
+    # print(f"Part One (input):  {part_one(INPUT, target=150)}")
+    # print()
+    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    # print(f"Part Two (input):  {part_two(INPUT)}")
+
+    random_tests()
+
+def random_tests():
+    ...
+
+       
+if __name__ == '__main__':
+    main()
