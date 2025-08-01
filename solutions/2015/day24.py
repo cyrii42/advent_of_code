6ffc15acd5c526b35cbdd4104a40c715bc641646
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-07-31 21:56:05 -0400

    starting 2015 day 24...

--- solutions/2015/day24.py
+++ solutions/2015/day24.py
@@ -1,23 +1,23 @@
-import pathlib
 import functools
 import itertools
-import operator
 import json
 import math
+import operator
 import os
+import pathlib
 import re
 from copy import deepcopy
 from dataclasses import dataclass, field
 from enum import Enum, IntEnum
 from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, NamedTuple, Optional, Protocol, Self, Literal
+from typing import Callable, Literal, NamedTuple, Optional, Protocol, Self
 
 import numpy as np
 import pandas as pd
 import polars as pl
 from alive_progress import alive_it
-from rich.table import Table
 from rich import print
+from rich.table import Table
 
 import advent_of_code as aoc
 
@@ -25,27 +25,34 @@ CURRENT_FILE = pathlib.Path(__file__)
 YEAR = int(CURRENT_FILE.parts[-2])
 DAY = int(CURRENT_FILE.stem.removeprefix('day'))
 
-EXAMPLE = aoc.get_example(YEAR, DAY)
+EXAMPLE="1\n2\n3\n4\n5\n7\n8\n9\n10\n11"
 INPUT = aoc.get_input(YEAR, DAY)
 DESCRIPTION = aoc.get_description(YEAR, DAY)
 
+@dataclass
+class PackageGroup:
+    packages: list[int]
+    weight: int = field(init=False)
+    qe: int = field(init=False)
+
+    def __post_init__(self):
+        self.weight = sum(package for package in self.packages)
+        self.qe = functools.reduce(operator.mul, self.packages)
+    
+
 def parse_data(data: str):
     line_list = data.splitlines()
+    return [int(x) for x in line_list]
     
 def part_one(data: str):
-    __ = parse_data(data)
-
-def part_two(data: str):
-    __ = parse_data(data)
+    package_list = parse_data(data)
 
 
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
-    print(f"Part One (input):  {part_one(INPUT)}")
-    print()
-    print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    print(f"Part Two (input):  {part_two(INPUT)}")
+    # print(f"Part One (input):  {part_one(INPUT)}")
+    # print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
