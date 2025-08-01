64948c8f20cccac089aa254218b37e175afe109a
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-01 16:20:43 -0400

    clean up comments & unused imports

--- solutions/2015/day25.py
+++ solutions/2015/day25.py
@@ -1,24 +1,8 @@
-import functools
-import itertools
-import json
-import math
-import operator
-import os
 import pathlib
-import re
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, Literal, NamedTuple, Optional, Protocol, Self
+from typing import Generator
 
 import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_it
 from rich import print
-from rich.table import Table
-
 import advent_of_code as aoc
 
 CURRENT_FILE = pathlib.Path(__file__)
@@ -26,11 +10,10 @@ YEAR = int(CURRENT_FILE.parts[-2])
 DAY = int(CURRENT_FILE.stem.removeprefix('day'))
 
 INPUT = aoc.get_input(YEAR, DAY)
-DESCRIPTION = aoc.get_description(YEAR, DAY)
 
 FIRST_CODE = 20151125   
-STEP_TWO = 252533       # multiply the previous code by 252533
-STEP_THREE = 33554393   # then keep the remainder from dividing that value by 33554393
+STEP_TWO = 252533
+STEP_THREE = 33554393
 
 def parse_data(data: str) -> tuple[int, int]:
     words = data.split(' ')
@@ -71,9 +54,9 @@ def part_two(data: str):
 
 
 def main():
-    # print(f"Part One (example):  {check_example_part_one()}")
+    check_example_part_one()
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
+
     # print(f"Part Two (example):  {part_two(EXAMPLE)}")
     # print(f"Part Two (input):  {part_two(INPUT)}")
 
