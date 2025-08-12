0d2151122e454570c996ef89f7893dea23cc0fc4
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-12 17:57:11 -0400

    solved 2016 day 3!

--- solutions/2016/day3.py
+++ solutions/2016/day3.py
@@ -1,23 +1,6 @@
-import functools
-import itertools
-import json
-import math
-import operator
-import os
 import pathlib
-import re
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, Literal, NamedTuple, Optional, Protocol, Self
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_it
 from rich import print
-from rich.table import Table
 
 import advent_of_code as aoc
 
@@ -25,10 +8,11 @@ CURRENT_FILE = pathlib.Path(__file__)
 YEAR = int(CURRENT_FILE.parts[-2])
 DAY = int(CURRENT_FILE.stem.removeprefix('day'))
 
-EXAMPLE = '5 10 25'
+EXAMPLE_PART_ONE = '5 10 25'
+EXAMPLE_PART_TWO = '101 301 501\n102 302 502\n103 303 503\n201 401 601\n202 402 602\n203 403 603'
 INPUT = aoc.get_input(YEAR, DAY)
 
-def parse_data(data: str) -> list[list[int]]:
+def parse_data_part_one(data: str) -> list[list[int]]:
     line_list = data.splitlines()
     return [[int(x) for x in line.strip().split()] for line in line_list]
 
@@ -38,20 +22,31 @@ def check_valid_triangle(nums: list[int]) -> bool:
     return a + b > c
     
 def part_one(data: str):
-    nums_list = parse_data(data)
+    nums_list = parse_data_part_one(data)
     return len([nums for nums in nums_list if check_valid_triangle(nums)])
 
+def parse_data_part_two(data: str) -> list[list[int]]:
+    line_list = data.splitlines()
+    line_list = [[int(x) for x in line.strip().split()] for line in line_list]
+
+    output_list = []
+    for i in range(0, len(line_list), 3):
+        output_list.append([line_list[i][0], line_list[i+1][0], line_list[i+2][0]])
+        output_list.append([line_list[i][1], line_list[i+1][1], line_list[i+2][1]])
+        output_list.append([line_list[i][2], line_list[i+1][2], line_list[i+2][2]])
+        
+    return output_list
+
 def part_two(data: str):
-    __ = parse_data(data)
-
-
+    nums_list = parse_data_part_two(data)
+    return len([nums for nums in nums_list if check_valid_triangle(nums)])
 
 def main():
-    print(f"Part One (example):  {part_one(EXAMPLE)}")
+    print(f"Part One (example):  {part_one(EXAMPLE_PART_ONE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    # print(f"Part Two (input):  {part_two(INPUT)}")
+    print()
+    print(f"Part Two (input):  {part_two(EXAMPLE_PART_TWO)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
