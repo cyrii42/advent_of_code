3c7dfd8d25e6b091ca15ee752a85b4a719ed399a
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-12 17:30:42 -0400

    solved 2016 day 2!

--- solutions/2016/day2.py
+++ solutions/2016/day2.py
@@ -35,23 +35,21 @@ def parse_data(data: str):
 def create_keypad_part_one() -> np.ndarray:
     return np.array([[y for y in range(x, x+3)] for x in [1, 4, 7]])
 
-def execute_instruction_set_part_one(start: tuple[int, int], instruction_set: str) -> tuple[int, int]:
-    x, y = start
+def execute_instruction_set_part_one(start: tuple[int, int], 
+                                     instruction_set: str) -> tuple[int, int]:
+    row, col = start
     for char in instruction_set:
         match char:
             case 'U':
-                x = max(x-1, 0)
+                row = max(row-1, 0)
             case 'R':
-                y = min(y+1, 2)
+                col = min(col+1, 2)
             case 'D':
-                x = min(x+1, 2)
+                row = min(row+1, 2)
             case 'L':
-                y = max(y-1, 0)
-    return (x, y)
+                col = max(col-1, 0)
+    return (row, col)
 
-def get_num_from_position(keypad: np.ndarray, position: tuple[int, int]) -> int:
-    x, y = position
-    return keypad[x, y]
     
 def part_one(data: str):
     keypad = create_keypad_part_one()
@@ -61,29 +59,66 @@ def part_one(data: str):
     start = (1, 1)
     for instruction_set in instructions_list:
         position = execute_instruction_set_part_one(start, instruction_set)
-        num = get_num_from_position(keypad, position)
+        row, col = position
+        num = keypad[row, col]
         output_str += str(num)
         start = position
     return output_str
 
 
 def create_keypad_part_two() -> np.ndarray:
-    ...
+    return np.array([[None, None, '1', None, None], 
+                     [None, '2', '3', '4', None], 
+                     ['5', '6', '7', '8', '9'], 
+                     [None, 'A', 'B', 'C', None],
+                     [None, None, 'D', None, None]])
+
+
+def execute_instruction_set_part_two(start: tuple[int, int], 
+                                     instruction_set: str,
+                                     keypad: np.ndarray) -> tuple[int, int]:
+    row, col = start
+    
+    # print(f"START: ({row}, {col})")
+    for char in instruction_set:
+        match char:
+            case 'U':
+                new_row = max(row-1, 0)
+                row = new_row if keypad[new_row, col] else row
+            case 'R':
+                new_col = min(col+1, 4)
+                col = new_col if keypad[row, new_col] else col
+            case 'D':
+                new_row = min(row+1, 4)
+                row = new_row if keypad[new_row, col] else row
+            case 'L':
+                new_col = max(col-1, 0)
+                col = new_col if keypad[row, new_col] else col
+        # print(f"{char}: ({row}, {col})")
+    return (row, col)
 
 def part_two(data: str):
     keypad = create_keypad_part_two()
     instructions_list = parse_data(data)
 
-    
+    output_str = ''
+    start = (2, 0)
+    for instruction_set in instructions_list:
+        position = execute_instruction_set_part_two(start, instruction_set, keypad)
+        row, col = position
+        num = keypad[row, col]
+        output_str += num
+        start = position
+    return output_str
 
 
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    # print(f"Part Two (input):  {part_two(INPUT)}")
+    print()
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
