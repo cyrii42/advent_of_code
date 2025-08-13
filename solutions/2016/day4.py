05861ce826d911e381a45f98376c2fb9bb2e5588
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-12 21:19:34 -0400

    solved 2016 day 4!

--- solutions/2016/day4.py
+++ solutions/2016/day4.py
@@ -1,23 +1,9 @@
-import functools
-import itertools
-import json
-import math
-import operator
-import os
 import pathlib
-import re
-from copy import deepcopy
 from dataclasses import dataclass, field
-from enum import Enum, IntEnum
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, Literal, NamedTuple, Optional, Protocol, Self
+from string import ascii_lowercase
+from typing import Self
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_it
 from rich import print
-from rich.table import Table
 
 import advent_of_code as aoc
 
@@ -34,15 +20,17 @@ class RoomInfo:
     id: int
     checksum: str
     is_real: bool = field(init=False)
+    real_name: str = field(init=False)
 
     def __post_init__(self):
         self.is_real = self.confirm_checksum()
+        self.real_name = self.decrypt_name()
 
     def confirm_checksum(self) -> bool:
         ''' A room is real (not a decoy) if the checksum is the five most common
         letters in the encrypted name, in order, with ties broken by alphabetization. '''
 
-        letters = sorted({char for char in self.name})
+        letters = sorted({char for char in self.name if char.isalpha()})
         letters = sorted(letters, key=lambda char: self.name.count(char), reverse=True)
         checksum = ''.join(char for char in letters[0:5])
         return checksum == self.checksum
@@ -51,8 +39,21 @@ class RoomInfo:
     def from_str(cls, line: str) -> Self:
         checksum = line[-6:].strip(']')
         id = int(''.join(char for char in line if char.isdigit()))
-        name = ''.join(char for char in line[0:-7] if char.isalpha())
+        name = ''.join(char for char in line[0:-11])
         return cls(name, id, checksum)
+
+    def decrypt_name(self) -> str:
+        output_str = ''
+        
+        for char in self.name:
+            if char == '-':
+                output_str += ' '
+            else:
+                idx = ascii_lowercase.index(char)
+                new_idx = (idx + self.id) % 26
+                output_str += ascii_lowercase[new_idx]
+                
+        return output_str  
 
 def parse_data(data_file: pathlib.Path) -> list[RoomInfo]:
     with open(data_file, 'r') as f:
@@ -65,16 +66,14 @@ def part_one(data: pathlib.Path):
     return sum(room.id for room in room_list if room.is_real)
 
 def part_two(data: pathlib.Path):
-    __ = parse_data(data)
-
-
+    room_list = parse_data(data)
+    return [room.id for room in room_list 
+            if room.real_name == 'northpole object storage'][0]
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    # print(f"Part Two (input):  {part_two(INPUT)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
