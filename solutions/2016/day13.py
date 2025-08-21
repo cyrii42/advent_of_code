3f6169668f1c852a72c4710b7bba19a77cfb6043
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-21 17:58:08 -0400

    still working on 2016 day 13...

--- solutions/2016/day13.py
+++ solutions/2016/day13.py
@@ -28,59 +28,117 @@ DAY = int(CURRENT_FILE.stem.removeprefix('day'))
 
 INPUT = aoc.get_input(YEAR, DAY)
 
-WIDTH = 7
-HEIGHT = 10
+NUM_ROWS = 50
+NUM_COLS = 50
+GOAL_ROW = 39
+GOAL_COL = 31
 
-class Direction(IntEnum):
-    NORTH = 0
-    EAST = 1
-    SOUTH = 2
-    WEST = 3
-    
-def characterize_location(x: int, y: int, fav_num: int) -> int:
-    step1 = (x*x + 3*x + 2*x*y + y + y*y)
-    step2 = step1 + fav_num
-    step3 = str(bin(step2))[2:]
-    step4 = len([char for char in step3 if char == '1'])
-    return 0 if step4 % 2 == 0 else 1
-        
-def part_one(data: str):
-    fav_num = int(data)
-    if data == '10':
-        goal_row = 4
-        goal_col = 7
-    else:
-        goal_row = 39
-        goal_col = 31
-    maze = Maze(fav_num, goal_row, goal_col)
+# class Direction(IntEnum):
+#     N  = 0
+#     NE = 1
+#     E  = 2
+#     SE = 3
+#     S  = 4
+#     SW = 5
+#     W  = 6
+#     NW = 7
+
+DIRECTIONS = {
+    'N':    (1, 0),
+    'NE':   (1, 1),
+    'E':    (0, 1),
+    'SE':   (-1, 1),
+    'S':    (-1, 0),
+    'SW':   (-1, -1),
+    'W':    (0, -1),
+    'NW':   (1, -1)
+}
 
 @dataclass
 class Maze:
+    num_rows: int
+    num_cols: int
     fav_num: int
     goal_row: int
     goal_col: int
     maze: np.ndarray = field(init=False)
 
     def __post_init__(self):
-        coordinates = [x for x in itertools.product(range(HEIGHT), range(WIDTH))]
-        self.maze = np.array([[0 for _ in range(HEIGHT)] for _ in range(WIDTH)])
+        self.maze = np.array([[0 for _ in range(self.num_cols)] 
+                              for _ in range(self.num_rows)])
+        coordinates = [x for x in 
+                       itertools.product(range(self.num_cols), 
+                                         range(self.num_rows))]      
         for pair in coordinates:
-            x, y = pair
-            self.maze[y, x] = characterize_location(x, y, self.fav_num)
+            col, row = pair
+            self.maze[row, col] = self.characterize_location(col, row)
 
+    def print(self):
+        print(self.maze)
 
+    def characterize_location(self, col: int, row: int) -> int:
+        step1 = (col*col + 3*col + 2*col*row + row + row*row)
+        step2 = step1 + self.fav_num
+        step3 = str(bin(step2))[2:]
+        step4 = len([char for char in step3 if char == '1'])
+        return 0 if step4 % 2 == 0 else 1
 
+    def get_neighbors(self, row: int, col: int) -> np.ndarray:
+        row_start = max(row-1, 0)
+        row_end = row+1
+        col_start = max(col-1, 0)
+        col_end = col+1
+
+        return self.maze[row_start:row_end+1, col_start:col_end+1]
     
     def traverse(self, 
                  row: int = 0, 
                  col: int = 0,
-                 count: int = 0):
+                 count: int = 0,
+                 points_visited: set[tuple[int, int]] = set()):
         if row == self.goal_row and col == self.goal_col:
             return count
+
+        points_visited.add((row, col))
+        count += 1
+
+        neighbors = self.get_neighbors(row, col)
+        for loc in DIRECTIONS.values():
+            try:
+                if not neighbors[loc]:
+                    continue
+            except IndexError:
+                print(f"Index error on {row}, {col} with loc {loc}")
+                continue
+
+            new_row = row + loc[0]
+            new_col = col + loc[1]
+            if (new_row, new_col) not in points_visited:
+                self.traverse(new_row, new_col, count, points_visited)
+        return count
+
+                
+
+def parse_data(data: str) -> Maze:
+    fav_num = int(data)
+    if data == '10':
+        num_rows = 7
+        num_cols = 10
+        goal_row = 4
+        goal_col = 7
+    else:
+        num_rows = NUM_ROWS
+        num_cols = NUM_COLS
+        goal_row = GOAL_ROW
+        goal_col = GOAL_COL
         
-        for dir in Direction:
-            ...
+    return Maze(num_rows, num_cols, fav_num, goal_row, goal_col)
         
+def part_one(data: str):
+    maze = parse_data(data)
+    # maze.print()
+    print(maze.traverse())
+    # print(maze.get_neighbors(1, 1))
     
 
 def part_two(data: str):
