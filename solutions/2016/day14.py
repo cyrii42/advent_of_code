3c291ed393cf9477b7f59f53bb68e033f896850b
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-22 22:59:15 -0400

    solved 2016 day 14!

--- solutions/2016/day14.py
+++ solutions/2016/day14.py
@@ -1,24 +1,8 @@
-import functools
 import hashlib
-import itertools
-import json
-import math
-import operator
-import os
 import pathlib
 import re
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, Literal, NamedTuple, Optional, Protocol, Self
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_it
 from rich import print
-from rich.table import Table
 
 import advent_of_code as aoc
 
@@ -31,25 +15,48 @@ INPUT = aoc.get_input(YEAR, DAY)
 
 TRIPLE_PATTERN = r'(.)\1{2,}'
 
+HASH_DICT = {}
+STRETCHED_HASH_DICT = {}
+
 def get_hash(s: str):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
 
-def generate_key(salt: str):
-    idx = 0
+def get_stretched_hash(s: str):
+    h = hashlib.md5(s.encode('utf-8')).hexdigest()
+    for _ in range(2016):
+        h = hashlib.md5(h.encode('utf-8')).hexdigest()
+    return h
 
+def generate_key(salt: str, part_two: bool = False):
+    idx = 0
+    func = get_stretched_hash if part_two else get_hash
     while True:
-        h = get_hash(f"{salt}{idx}")
+        h = func(f"{salt}{idx}")
         m = re.search(TRIPLE_PATTERN, h)
         if m:
             char = m[0][0]
-            if verify_key(idx, char, salt):
+            if verify_key(idx, char, salt, part_two=part_two):
                 yield idx
         idx += 1
 
-def verify_key(idx: int, char: str, salt: str) -> bool:
+def verify_key(idx: int, char: str, salt: str, 
+               part_two: bool = False) -> bool:
     pattern = ''.join(char for _ in range(5))
-    return any(pattern in get_hash(f"{salt}{x}") 
-               for x in range(idx+1, idx+1001))
+    func = get_stretched_hash if part_two else get_hash
+    d = STRETCHED_HASH_DICT if part_two else HASH_DICT
+    
+    for x in range(idx+1, idx+1001):
+        s = f"{salt}{x}"
+        if s in d.keys():
+            h = d[s]
+        else:
+            h = func(s)
+            d[s] = h
+            
+        if pattern in h:
+            return True
+
+    return False
     
 def part_one(data: str):
     salt = data
@@ -58,17 +65,18 @@ def part_one(data: str):
         next(gen)
     return next(gen)
     
-
 def part_two(data: str):
-    ...
-
-
+    salt = data
+    gen = generate_key(salt, part_two=True)
+    for _ in range(63):
+        next(gen)
+    return next(gen)
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
-    # print(f"Part Two (input):  {part_two(INPUT)}")
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
 
