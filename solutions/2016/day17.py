b37ed830c03daf3b74d11f5526cb3f117e7504f1
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-08-23 20:42:07 -0400

    solved 2016 day 17 part one; stuck on part two

--- solutions/2016/day17.py
+++ solutions/2016/day17.py
@@ -7,6 +7,7 @@ import operator
 import os
 import pathlib
 import re
+from collections import deque
 from copy import deepcopy
 from dataclasses import dataclass, field
 from enum import Enum, IntEnum, StrEnum
@@ -24,14 +25,20 @@ CURRENT_FILE = pathlib.Path(__file__)
 YEAR = int(CURRENT_FILE.parts[-2])
 DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
 
-EXAMPLE_TESTS = [
+EXAMPLE_TESTS_PART_ONE = [
     ('ihgpwlah', 'DDRRRD'),
     ('kglvqrro', 'DDUDRLRRUDRD'),
     ('ulqzkmiv', 'DRURDRUDDLLDLUURRDULRLDUUDDDRR')
 ]
+EXAMPLE_TESTS_PART_TWO = [
+    ('ihgpwlah', 370),
+    # ('kglvqrro', 492),
+    # ('ulqzkmiv', 830)
+]
 INPUT = aoc.get_input(YEAR, DAY)
 
-GOAL = (3, 3)
+START = (0, 0)
+END = (3, 3)
 
 MAX_X = 3
 MAX_Y = 3
@@ -51,6 +58,13 @@ class Direction(IntEnum):
     L = 2
     R = 3
 
+DIRECTION_DELTAS = {
+    Direction.U: (0, -1),
+    Direction.D: (0, 1),
+    Direction.L: (-1, 0),
+    Direction.R: (1, 0),
+}
+
 def get_hash(s: str):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
 
@@ -58,94 +72,127 @@ def get_hash(s: str):
 class Room():
     x: int
     y: int
-    door_codes: str
+    path: str
+    passcode: str
+    door_codes: str = field(init=False)
     door_status: dict[Direction, bool] = field(init=False)
-    valid_neighbors: list[tuple[int, int]] = field(init=False)
+
+    @property
+    def num_open_doors(self) -> int:
+        return len([x for x in self.door_status.values() if x])
+
+    def is_part_two_end(self) -> bool:
+        return (
+            ((self.x, self.y) == (3, 2) and self.only_direction(Direction.D))
+            or ((self.x, self.y) == (2, 3) and self.only_direction(Direction.R))
+        )
+
+    def only_direction(self, test_dir: Direction) -> bool:
+        return self.door_status[test_dir] and self.num_open_doors == 1
 
     def __post_init__(self):
+        self.door_codes = get_hash(f"{self.passcode}{self.path}")
         self.door_status = {d: self.is_open(d) for d in Direction}
-        self.valid_neighbors = self.get_valid_neighbors()
 
-    def get_valid_neighbors(self) -> list[tuple[int, int]]:
-        
+    @property
+    def hash(self) -> str:
+        return f"{self.x}_{self.y}_{self.door_codes}"
+
+    def get_valid_neighbors(self) -> list["Room"]:
+        output_list = []
+        for dir, delta in DIRECTION_DELTAS.items():
+            if self.is_open(dir):
+                output_list.append(Room(self.x+delta[0], 
+                                        self.y+delta[1],
+                                        path=self.path + dir.name,
+                                        passcode=self.passcode))
+        return output_list
 
     def is_open(self, dir: Direction) -> bool:
         if self.door_codes[dir.value] not in ['b', 'c', 'd', 'e', 'f']:
             return False
-        
-        x, y = (self.x, self.y)
+
         match dir:
             case Direction.U:
-                return y > 0
+                return self.y > 0
             case Direction.D:
-                return y < MAX_Y
+                return self.y < MAX_Y
             case Direction.L:
-                return x > 0
+                return self.x > 0
             case Direction.R:
-                return x < MAX_X
+                return self.x < MAX_X
     
 @dataclass
 class Maze:
     passcode: str
-    current_location: tuple[int, int] = (0, 0)
-    current_room: Room = field(init=False)
     path: str = field(default_factory=str)
-
-    def __post_init__(self):
-        self.current_room = self.get_current_room()
     
-    def get_current_room(self) -> Room:
-        x, y = self.current_location
-        return Room(x=x, 
-                    y=y,
-                    door_codes=get_hash(
-                        f"{self.passcode}{self.path}"))
+    def get_room(self, x: int, y: int) -> Room:
+        return Room(x=x, y=y, path=self.path, passcode=self.passcode)
 
-    def get_location_from_path(self) -> tuple[int, int]:
-        x, y = (0, 0)
-        for char in self.path:
-            match Direction[char]:
-                case Direction.U:
-                    if y == 0:
-                        raise OutOfMaze
-                    else:
-                        y -= 1
-                case Direction.D:
-                    if y == MAX_Y:
-                        raise OutOfMaze
-                    else:
-                        y += 1
-                case Direction.L:
-                    if x == 0:
-                        raise OutOfMaze
-                    else:
-                        x -= 1
-                case Direction.R:
-                    if x == MAX_X:
-                        raise OutOfMaze
-                    else:
-                       x += 1
-        return (x, y)
+    def get_part_one_answer(self) -> str:
+        start = self.get_room(*START)
+        end = END
+        queue = deque([(start, [start])])
+
+        visited = set()
+        visited.add(start.hash)
+
+        while queue:
+            room, path = queue.popleft()
+            if (room.x, room.y) == end:
+                return room.path
+            for neighbor in room.get_valid_neighbors():
+                if neighbor.hash not in visited:
+                    new_path = path + [neighbor]
+                    queue.append((neighbor, new_path))
+                    visited.add(neighbor.hash)
+        return ''
+
+    def get_part_two_answer(self) -> int:
+        all_paths = []
+        start = self.get_room(*START)
+        end = END
+        queue = deque([(start, [start])])
+
+        visited = set()
+        visited.add((start.x, start.y))
+
+        while queue:
+            room, path = queue.popleft()
+            if (room.x, room.y) == end:
+                all
+            for neighbor in room.get_valid_neighbors():
+                if neighbor.hash not in visited:
+                    new_path = path + [neighbor]
+                    queue.append((neighbor, new_path))
+                    visited.add(neighbor.hash)
+        return -1
 
 def part_one_tests():
-    for i, example in enumerate(EXAMPLE_TESTS, start=1):
+    for i, example in enumerate(EXAMPLE_TESTS_PART_ONE, start=1):
         passcode, answer = example
-        print(f"Test #{i} ({passcode}): {part_one(passcode) == answer}")
-    
+        print(f"Test #{i} ({passcode}): {part_one(passcode) == answer} ({part_one(passcode)})")
+
+def part_two_tests():
+    for i, example in enumerate(EXAMPLE_TESTS_PART_TWO, start=1):
+        passcode, answer = example
+        print(f"Test #{i} ({passcode}): {part_two(passcode) == answer} ({part_two(passcode)})")
+        
 def part_one(data: str):
     maze = Maze(passcode=data)
-    print(maze.get_room_status())
+    return maze.get_part_one_answer()
     
 
 def part_two(data: str):
-    ...
+    maze = Maze(passcode=data)
+    return maze.get_part_two_answer()
 
 
 def main():
     # part_one_tests()
-    print(f"Part One (input):  {part_one(INPUT)}")
-    # print()
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    # print(f"Part One (input):  {part_one(INPUT)}")
+    part_two_tests()
     # print(f"Part Two (input):  {part_two(INPUT)}")
 
     random_tests()
