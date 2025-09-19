5a9b39c4d8a5dac3c523d6a2ef048283e4bef3ec
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-09-19 11:21:00 -0400

    solved 2017 day 17 part 2

--- solutions/2017/day17.py
+++ solutions/2017/day17.py
@@ -1,23 +1,7 @@
-import functools
-import hashlib
 import itertools
-import json
-import math
-import operator
-import os
-import re
-from collections import defaultdict, deque
-from copy import deepcopy
 from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
 from pathlib import Path
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, Literal, NamedTuple, Optional, Protocol, Self
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_bar, alive_it
 from rich import print
 
 import advent_of_code as aoc
@@ -46,18 +30,7 @@ class Spinlock:
     def buffer_size(self) -> int:
         return len(self.buffer)
 
-    def run_simulation(self):
-        ''' It starts with a circular buffer containing only the value 0, 
-        which it marks as the current position. It then steps forward through 
-        the circular buffer some number of steps (your puzzle input) before
-        inserting the first new value, 1, after the value it stopped on. The 
-        inserted value becomes the current position. Then, it steps forward 
-        from there the same number of steps, and wherever it stops, inserts 
-        after it the second new value, 2, and uses that as the new current 
-        position again.'''
-
-        num_insertions = 2017
-
+    def run_simulation(self, num_insertions: int = 2017) -> None:
         for _ in range(num_insertions):
             self.ptr = (self.ptr + self.num_steps) % self.buffer_size
             self.insert_new_number(next(self.counter))
@@ -76,13 +49,16 @@ class Spinlock:
         return self.buffer[idx_2017+1]
 
     def solve_part_two(self) -> int:
-        ...
-
-def modulo_tests():
-    n = 0
-    mod = 2
-    for x in range(1, 4):
-        print(f"{n} + {x} mod {mod} = {(n+x)%mod}")
+        pretend_buffer_size = 1
+        ans = 0
+        for _ in range(50_000_000):
+            self.ptr = (self.ptr + self.num_steps) % pretend_buffer_size
+            c = next(self.counter)
+            if self.ptr == 0:
+                ans = c
+            pretend_buffer_size += 1
+            self.ptr = self.ptr + 1
+        return ans
     
 def part_one(data: str):
     num_steps = int(data)
@@ -94,18 +70,10 @@ def part_two(data: str):
     spinlock = Spinlock(num_steps=num_steps)
     return spinlock.solve_part_two()
 
-
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
-    # print(f"Part Two (example):  {part_two(EXAMPLE)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
-
-    # random_tests()
-
-def random_tests():
-    modulo_tests()
-
        
 if __name__ == '__main__':
     main()
