79c04ca84c5e4f536ae4c86291dab5dcbddc2671
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-09-20 13:01:01 -0400

    still working on 2017 day 19...

--- solutions/2017/day19.py
+++ solutions/2017/day19.py
@@ -29,6 +29,9 @@ DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
 EXAMPLE = aoc.get_example(YEAR, DAY)
 INPUT = aoc.get_input(YEAR, DAY)
 
+class TooManyNeigbors(Exception):
+    pass
+
 class Direction(Enum):
     NORTH = 0
     EAST = 1
@@ -36,9 +39,9 @@ class Direction(Enum):
     WEST = 3
 
 DIRECTION_DELTAS = {
-    Direction.NORTH: (0, 1),
+    Direction.NORTH: (0, -1),
     Direction.EAST: (1, 0),
-    Direction.SOUTH: (0, -1),
+    Direction.SOUTH: (0, 1),
     Direction.WEST: (-1, 0),
 }
 
@@ -83,27 +86,59 @@ def create_graph(node_list: list[Node]) -> dict[Node, list[Node]]:
 def find_start(node_list: list[Node]) -> Node:
     return next(node for node in node_list if node.y == 0)
 
+def determine_next_direction(node: Node, next_node: Node) -> Direction:
+    delta_x = next_node.x - node.x
+    delta_y = next_node.y - node.y
+    match (delta_x, delta_y):
+        case (0, -1):
+            return Direction.NORTH
+        case (1, 0):
+            return Direction.EAST
+        case (0, 1):
+            return Direction.SOUTH
+        case (-1, 0):
+            return Direction.WEST
+
 def walk_path(graph: dict[Node, list[Node]],
               node: Node,
               dir: Direction = Direction.SOUTH,
-              visited: Optional[list[Node]] = None
+              visited: Optional[list[tuple[Node, Direction]]] = None
               ) -> list[str]:
     if not visited:
         visited = []
+
+    visited.append((node, dir))
+
+    if node.node_type == NodeType.CORNER:
+        x, y = (node.x, node.y)
+        valid_neighbors = [n for n in graph[node] if (n, dir) not in visited]
+        if len(valid_neighbors) > 1:
+            print(node, valid_neighbors)
+            raise TooManyNeigbors
+        try:
+            next_node = next(n for n in graph[node] if (n, dir) not in visited)
+            next_dir = determine_next_direction(node, next_node)
+            print(f"FOUND A CORNER - next node: {next_node} - next dir: {next_dir}")
+            walk_path(graph, next_node, next_dir, visited)
+        except StopIteration:
+            print('end')
 
     if node.node_type != NodeType.CORNER:
         x, y = (node.x, node.y)
         delta_x, delta_y = DIRECTION_DELTAS[dir]
         next_x = x + delta_x
         next_y = y + delta_y
-
-    try:
-        next_node = next(n for n in graph[node] if n.x == next_x and n.y == next_y)
-    except StopIteration:
         try:
-            ...
-        except StopIteration:
-            return [node.char for node in visited if node.char.isalpha()]
+            # print(f"Neighbors: {graph[node]}")
+            # print(f"Next X: {next_x}, Next Y: {next_y}")
+            next_node = next(n for n in graph[node] 
+                             if n.x == next_x and n.y == next_y)
+            # print(f"Next: {next_node}")
+            walk_path(graph, next_node, dir, visited)
+        except (StopIteration, RecursionError):
+            print('end')
+    return visited
+    return [node.char for node, _ in visited if node.char.isalpha()]
     
 
 # def walk_path(graph: dict[Node, list[Node]],
@@ -173,6 +208,7 @@ def part_one(data: str):
     node_list = parse_data(data)
     graph = create_graph(node_list)
     start = find_start(node_list)
+    print(f"START: {start}")
     return walk_path(graph, start)
     
 
@@ -183,7 +219,7 @@ def part_two(data: str):
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
-    # print(f"Part One (input):  {part_one(INPUT)}")
+    print(f"Part One (input):  {part_one(INPUT)}")
     # print(f"Part Two (example):  {part_two(EXAMPLE)}")
     # print(f"Part Two (input):  {part_two(INPUT)}")
 
