f994770e2bd36897c3099b449ed2f4eb99200ffc
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-09-21 21:51:49 -0400

    started 2017 day 21

---
+++ solutions/2017/day21.py
@@ -0,0 +1,56 @@
+import functools
+import itertools
+import operator
+import hashlib
+import json
+import math
+import os
+import re
+from pathlib import Path
+from collections import deque, defaultdict
+from copy import deepcopy
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum, StrEnum
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+from typing import Callable, NamedTuple, Optional, Protocol, Self, Literal, Generator
+
+import numpy as np
+import pandas as pd
+import polars as pl
+from alive_progress import alive_it, alive_bar
+from rich import print
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
+
+EXAMPLE = aoc.get_example(YEAR, DAY)
+INPUT = aoc.get_input(YEAR, DAY)
+
+def parse_data(data: str):
+    line_list = data.splitlines()
+    
+def part_one(data: str):
+    __ = parse_data(data)
+
+def part_two(data: str):
+    __ = parse_data(data)
+
+
+
+def main():
+    print(f"Part One (example):  {part_one(EXAMPLE)}")
+    print(f"Part One (input):  {part_one(INPUT)}")
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
+
+    random_tests()
+
+def random_tests():
+    ...
+
+       
+if __name__ == '__main__':
+    main()
