90ef3e828b5011b85ef6cdcca1e47f034630b10f
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-09-27 17:06:47 -0400

    still working on 2018 day 3

--- solutions/2018/day03.py
+++ solutions/2018/day03.py
@@ -30,12 +30,42 @@ DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
 EXAMPLE = '#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2\n'
 INPUT = aoc.get_input(YEAR, DAY)
 
-class Rectangle(NamedTuple):
+class Point(NamedTuple):
+    row: int
+    col: int
+
+@dataclass
+class Rectangle:
     claim_id: int
     inches_from_left: int
     inches_from_top: int
     width: int
     height: int
+
+    def get_coordinates(self) -> list[Point]:
+        start = Point(row=self.inches_from_top,
+                      col=self.inches_from_left)
+        output_list = []
+    
+        for row in range(self.height):
+            for col in range(self.width):
+                pt_row = start.row + row
+                pt_col = start.col + col
+                output_list.append(Point(pt_row, pt_col))
+        return output_list
+
+def make_rectangle_dict(rectangle_list: list[Rectangle]
+                        ) -> dict[int, list[Point]]:
+    return {rect.claim_id: rect.get_coordinates() for rect in rectangle_list}
+
+def count_overlaps(rectangle_dict: dict[int, list[Point]]) -> int:
+    raw_count = sum(len(v) for v in rectangle_dict.values())
+
+    point_set = set()
+    for point_list in rectangle_dict.values():
+        point_set.update({p for p in point_list})
+
+    return raw_count - len(point_set)   
 
 def parse_data(data: str) -> list[Rectangle]:
     line_list = data.splitlines()
@@ -43,21 +73,24 @@ def parse_data(data: str) -> list[Rectangle]:
     for line in line_list:
         id, _ , inches, rect = line.split(' ')
         id = int(id.removeprefix('#'))
-        inches_from_left, inches_from_top = [int(x) for x in inches.removesuffix(':').split(',')]
+        inches_from_left, inches_from_top = (
+            [int(x) for x in inches.removesuffix(':').split(',')])
         width, height = [int(x) for x in rect.split('x')]
-        output_list.append(Rectangle(id, inches_from_left, inches_from_top, width, height))
+        output_list.append(Rectangle(id, inches_from_left, 
+                                     inches_from_top, width, height))
     return output_list
     
 def part_one(data: str):
     rectangle_list = parse_data(data)
-    print(rectangle_list)
+    rectangle_dict = make_rectangle_dict(rectangle_list)
+    return count_overlaps(rectangle_dict)
 
 def part_two(data: str):
     ...
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
-    # print(f"Part One (input):  {part_one(INPUT)}")
+    print(f"Part One (input):  {part_one(INPUT)}")
     # print(f"Part Two (example):  {part_two(EXAMPLE)}")
     # print(f"Part Two (input):  {part_two(INPUT)}")
 
