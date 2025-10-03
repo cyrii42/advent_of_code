903fb9eaa63ea70bd53baba2b7b37164f3c93431
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-10-02 21:06:36 -0400

    solved 2018 day 7

--- solutions/2018/day07.py
+++ solutions/2018/day07.py
@@ -1,26 +1,7 @@
-import functools
-import hashlib
-import heapq
-import itertools
-import json
-import math
-import operator
-import os
-import re
-import sys
-from collections import defaultdict, deque
-from copy import deepcopy
+from collections import defaultdict
 from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
 from pathlib import Path
-from queue import PriorityQueue
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, NamedTuple, Optional, Self
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_bar, alive_it
 from rich import print
 
 import advent_of_code as aoc
@@ -78,26 +59,26 @@ def part_one(data: str):
     prereq_graph = create_prereq_graph(pair_list)
     return find_correct_order(order_graph, prereq_graph)
 
-
-
 @dataclass
 class Elf:
     id: int
     step: str
-    time_remaining: int = field(init=False)
+    example: bool = field(default=False, repr=False) 
+    t: int = field(init=False)
 
     def __post_init__(self):
         assert 1 <= self.id <= 5
         assert len(self.step) == 1 
         assert self.step.isupper()
-        self.time_remaining = 60 + (ord(self.step) - 64)    ## add 60 for input
+        constant = 0 if self.example else 60
+        self.t = constant + (ord(self.step) - 64)
 
     @property
     def finished(self) -> bool:
-        return self.time_remaining <= 0
+        return self.t <= 0
 
     def increment(self) -> None:
-        self.time_remaining -= 1
+        self.t -= 1
 
 class ElfSlotsFull(Exception):
     pass
@@ -105,21 +86,24 @@ class ElfSlotsFull(Exception):
 @dataclass
 class ElfGroup:
     elf_list: list[Elf] = field(default_factory=list)
+    example: bool = field(default=False, repr=False) 
+    time_elapsed: int = 0
 
     @property
     def full(self) -> bool:
-        return len(self.elf_list) >= 5   ## 2 for example, 5 for input
+        limit = 2 if self.example else 5
+        return len(self.elf_list) >= limit
 
     @property
     def steps_processing(self) -> list[str]:
         return [elf.step for elf in self.elf_list]
 
     def add_elf(self, step: str) -> None:
-        if len(self.elf_list) >= 5:
+        if self.full:
             raise ElfSlotsFull
 
         next_elf_id = len(self.elf_list) + 1
-        self.elf_list.append(Elf(next_elf_id, step))
+        self.elf_list.append(Elf(next_elf_id, step, self.example))
 
     def delete_elf(self, elf: Elf) -> None:
         self.elf_list.remove(elf)
@@ -129,56 +113,58 @@ class ElfGroup:
 
     def get_finished_steps(self) -> list[str]:
         output_list = []
+        elves_to_delete = []
         
         for elf in self.elf_list:
             elf.increment()
             if elf.finished:
                 output_list.append(elf.step)
-                self.delete_elf(elf)
-            
+                elves_to_delete.append(elf)
+
+        for elf in elves_to_delete:
+            self.delete_elf(elf)
+
+        self.time_elapsed += 1
         return output_list
         
-
-    
-
-def find_correct_order_part_two(order_graph: dict[str, set[str]], 
-                                prereq_graph: dict[str, set[str]]
-                                ) -> str:
+def count_required_steps(order_graph: dict[str, set[str]], 
+                         prereq_graph: dict[str, set[str]],
+                         example: bool = False
+                         ) -> int:
     ''' If multiple steps are available, workers should still begin them in 
     alphabetical order. Each step takes 60 seconds plus an amount corresponding
     to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, 
     while step Z takes 60+26=86 seconds. No time is required between steps. '''
 
-    start = sorted(p for p in order_graph.keys() 
-                   if p not in prereq_graph.keys())[0]
     all_nodes = {n for n in order_graph.keys()} | {n for n in prereq_graph.keys()}
     visited = []
 
-    elf_group = ElfGroup()
+    elf_group = ElfGroup(example=example)
 
     while True:
         next_node_list = sorted([node for node in all_nodes 
-                                 if node not in visited 
-                                 and all(n in visited for n in prereq_graph[node])
-                                 and node not in elf_group.steps_processing], 
+                                  if node not in visited 
+                                  and all(n in visited for n in prereq_graph[node])
+                                  and node not in elf_group.steps_processing], 
                                 reverse=True)
 
-        if next_node_list and not elf_group.full:
+        while next_node_list and not elf_group.full:
             next_node = next_node_list.pop()
             elf_group.add_elf(next_node)
 
-        visited += elf_group.get_finished_steps()
-        elf_group.list_elves()
-        
+        finished = elf_group.get_finished_steps()
+        for node in sorted(finished):
+            visited.append(node)
+
         if len(visited) == len(all_nodes):
-            return ''.join(node for node in visited)
-    
+            return elf_group.time_elapsed
 
 def part_two(data: str):
     pair_list = parse_data(data)
     order_graph = create_order_graph(pair_list)
     prereq_graph = create_prereq_graph(pair_list)
-    return find_correct_order_part_two(order_graph, prereq_graph)
+    example = True if data == EXAMPLE else False
+    return count_required_steps(order_graph, prereq_graph, example)
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
@@ -186,13 +172,5 @@ def main():
     print(f"Part Two (example):  {part_two(EXAMPLE)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
 
-    random_tests()
-
-
-
-def random_tests():
-    ...
-
-       
 if __name__ == '__main__':
     main()
