c9a0ec0f6a2bcd99942a46f0eff8062371feb72b
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-01 19:12:45 -0500

    clean up unused imports

--- solutions/2019/day02.py
+++ solutions/2019/day02.py
@@ -1,24 +1,6 @@
-import functools
-import hashlib
 import itertools
-import json
-import math
 import operator
-import os
-import sys
-import re
-from collections import defaultdict, deque
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
 from pathlib import Path
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, NamedTuple, Optional, Self
-
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_bar, alive_it
 from rich import print
 
 import advent_of_code as aoc
@@ -69,18 +51,10 @@ def part_two(data: str):
         if program[0] == 19690720:
             return f"{noun}{verb}"
 
-
-
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
     print(f"Part One (input):  {part_one(INPUT)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
 
-    random_tests()
-
-def random_tests():
-    ...
-
-       
 if __name__ == '__main__':
     main()
