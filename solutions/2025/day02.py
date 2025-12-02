63459b26064f6cd0e6ff680f872d456f8d4e0930
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-02 12:00:57 -0500

    solved 2025 day 2

--- solutions/2025/day02.py
+++ solutions/2025/day02.py
@@ -1,24 +1,7 @@
-import functools
-import hashlib
-import itertools
-import json
 import math
-import operator
-import os
-import sys
-import re
-from collections import defaultdict, deque
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
 from pathlib import Path
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, NamedTuple, Optional, Self, Any
+from typing import Callable
 
-import numpy as np
-import pandas as pd
-import polars as pl
-from alive_progress import alive_bar, alive_it
 from rich import print
 
 import advent_of_code as aoc
@@ -27,19 +10,52 @@ CURRENT_FILE = Path(__file__)
 YEAR = int(CURRENT_FILE.parts[-2])
 DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
 
-EXAMPLE = aoc.get_example(YEAR, DAY)
+EXAMPLE = aoc.get_example(YEAR, DAY).replace('\n', '')
 INPUT = aoc.get_input(YEAR, DAY)
 
-def parse_data(data: str):
-    line_list = data.splitlines()
+def parse_data(data: str) -> list[tuple[int, int]]:
+    pairs = [pair.split('-') for pair in data.split(',')]
+    return [(int(pair[0]), int(pair[1])) for pair in pairs]
+
+def validate_id_part_one(id: int) -> bool:
+    id_str = str(id)
+
+    if len(id_str) % 2 != 0:
+        return True
+
+    midpoint = len(id_str) // 2
+    if id_str[:midpoint] == id_str[midpoint:]:
+        return False
+
+    return True
+
+def validate_id_part_two(id: int) -> bool:
+    id_str = str(id)
     
+    for i in range(1, (math.ceil(len(id_str) / 2) + 1)):
+        substring = id_str[0:i]
+        count = id_str.count(substring)
+        if (count > 1 and count == math.ceil(len(id_str) / len(substring))):
+            return False
+
+    return True
+
+def get_answer(data: str, validation_fn: Callable[[int], bool]):
+    pairs = parse_data(data)
+
+    invalid_id_list = []
+    for pair in pairs:
+        start, end = pair
+        for id in range(start, end+1):
+            if not validation_fn(id):
+                invalid_id_list.append(id)
+    return sum(invalid_id_list)
+
 def part_one(data: str):
-    __ = parse_data(data)
+    return get_answer(data, validate_id_part_one)
 
 def part_two(data: str):
-    __ = parse_data(data)
-
-
+    return get_answer(data, validate_id_part_two)
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
@@ -47,11 +63,5 @@ def main():
     print(f"Part Two (example):  {part_two(EXAMPLE)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
 
-    random_tests()
-
-def random_tests():
-    ...
-
-       
 if __name__ == '__main__':
     main()
