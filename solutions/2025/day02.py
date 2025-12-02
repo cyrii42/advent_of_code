44c193c346927220d1dc6ba044f0c86b14bec00a
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-01 23:43:10 -0500

    started 2025 day 2

---
+++ solutions/2025/day02.py
@@ -0,0 +1,57 @@
+import functools
+import hashlib
+import itertools
+import json
+import math
+import operator
+import os
+import sys
+import re
+from collections import defaultdict, deque
+from copy import deepcopy
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum, StrEnum
+from pathlib import Path
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+from typing import Callable, Generator, NamedTuple, Optional, Self, Any
+
+import numpy as np
+import pandas as pd
+import polars as pl
+from alive_progress import alive_bar, alive_it
+from rich import print
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
+
+EXAMPLE = aoc.get_example(YEAR, DAY)
+INPUT = aoc.get_input(YEAR, DAY)
+
+def parse_data(data: str):
+    line_list = data.splitlines()
+    
+def part_one(data: str):
+    __ = parse_data(data)
+
+def part_two(data: str):
+    __ = parse_data(data)
+
+
+
+def main():
+    print(f"Part One (example):  {part_one(EXAMPLE)}")
+    print(f"Part One (input):  {part_one(INPUT)}")
+    print(f"Part Two (example):  {part_two(EXAMPLE)}")
+    print(f"Part Two (input):  {part_two(INPUT)}")
+
+    random_tests()
+
+def random_tests():
+    ...
+
+       
+if __name__ == '__main__':
+    main()
