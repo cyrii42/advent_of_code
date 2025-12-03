005f237eb0d61a6e5ddef1fa76bc6c5d84859911
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-03 00:02:34 -0500

    solved 2019 day 7 part 1

---
+++ solutions/2019/day07.py
@@ -0,0 +1,212 @@
+import itertools
+from dataclasses import dataclass, field
+from collections import deque
+from pathlib import Path
+from typing import Callable, Any
+from rich import print
+
+import advent_of_code as aoc
+
+CURRENT_FILE = Path(__file__)
+YEAR = int(CURRENT_FILE.parts[-2])
+DAY = int(CURRENT_FILE.stem.removeprefix('day')[0:2])
+
+TESTS_PART_ONE = [
+    ('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', 43210),  # 4,3,2,1,0
+    ('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0', 54321), # 0,1,2,3,4
+    ('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', 65210) # 1,0,4,3,2
+]
+TESTS_PART_TWO = [
+    ('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5', 139629729), # 9,8,7,6,5
+    ('3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10', 18216) # 9,7,8,5,6
+]
+INPUT = aoc.get_input(YEAR, DAY)
+
+@dataclass
+class Computer:
+    program: list[int] = field(repr=False)
+    inputs: list[int]
+    output_list: list[int] = field(default_factory=list)
+    ptr: int = 0
+
+    def __post_init__(self):
+        self.opcode_dict: dict[int, Callable] = {
+            1: self.add,
+            2: self.mul,
+            3: self.input,
+            4: self.output,
+            5: self.jump_if_true,
+            6: self.jump_if_false,
+            7: self.less_than,
+            8: self.equals,
+            99: self.end_program,
+        }
+
+    def parse_instruction(self, instruction: int) -> tuple[Callable, int, int, int]:
+        instruction_str = f"{instruction:05d}"
+        opcode = int(instruction_str[3:])
+        fn = self.opcode_dict[opcode]
+        mode_c, mode_b, mode_a = [int(x) for x in list(instruction_str[0:3])]
+        return (fn, mode_a, mode_b, mode_c)    
+
+    def get_value(self, num: int, mode: int) -> int:
+        return num if mode == 1 else self.program[num]   
+
+    def execute_program(self):
+        while True:
+            inst = self.program[self.ptr]
+            fn, mode_a, mode_b, _ = self.parse_instruction(inst)
+
+            if fn == self.end_program:
+                break
+            elif fn in [self.add, self.mul, self.less_than, self.equals]:
+                a, b, c = [self.program[self.ptr + x] for x in range(1, 4)]
+                val_a = self.get_value(a, mode_a)
+                val_b = self.get_value(b, mode_b)
+                val_c = c  # Write parameters will never be in immediate mode
+                fn(val_a, val_b, val_c)
+                self.ptr += 4
+            elif fn in [self.jump_if_true, self.jump_if_false]:
+                a, b = [self.program[self.ptr + x] for x in range(1, 3)]
+                val_a = self.get_value(a, mode_a)
+                val_b = self.get_value(b, mode_b)
+                fn(val_a, val_b)
+            elif fn == self.input:
+                a = self.program[self.ptr + 1]
+                val_a = a
+                fn(val_a)
+                self.ptr += 2 
+            elif fn == self.output:
+                a = self.program[self.ptr + 1]
+                val_a = self.get_value(a, mode_a)
+                fn(val_a)
+                self.ptr += 2    
+
+    def add(self, a: int, b: int, c: int) -> None:
+        ''' Adds together numbers read from two positions and stores
+        the result in a third position'''
+        
+        self.program[c] = a + b
+
+    def mul(self, a: int, b: int, c: int) -> None:
+        ''' Multiplies numbers read from two positions and stores
+        the result in a third position'''
+        
+        self.program[c] = a * b
+
+    def input(self, a: int) -> None:
+        ''' Takes a single integer as input and saves it to the position 
+        given by its only parameter. For example, the instruction 3,50 would 
+        take an input value and store it at address 50.'''
+        
+        self.program[a] = self.inputs.pop(0)
+
+    def output(self, a: int) -> None:
+        ''' Outputs the value of its only parameter. For example, the 
+        instruction 4,50 would output the value at address 50. '''
+        
+        self.output_list.append(a)
+
+    def jump_if_true(self, a: int, b: int) -> None:
+        ''' If the first parameter is non-zero, sets the instruction pointer 
+        to the value from the second parameter. Otherwise, does nothing. '''
+        
+        if a != 0:
+            self.ptr = b
+        else:
+            self.ptr += 3
+
+    def jump_if_false(self, a: int, b: int) -> None:
+        ''' If the first parameter is zero, sets the instruction pointer 
+        to the value from the second parameter. Otherwise, does nothing. '''
+        
+        if a == 0:
+            self.ptr = b
+        else:
+            self.ptr += 3
+
+    def less_than(self, a: int, b: int, c: int) -> None:
+        ''' if the first parameter is less than the second parameter, stores 1 
+        in the position given by the third parameter. Otherwise, stores 0. '''
+        
+        if a < b:
+            self.program[c] = 1
+        else:
+            self.program[c] = 0
+
+    def equals(self, a: int, b: int, c: int) -> None:
+        ''' if the first parameter is equal to the second parameter, stores 1 
+        in the position given by the third parameter. Otherwise, stores 0. '''
+        
+        if a == b:
+            self.program[c] = 1
+        else:
+            self.program[c] = 0
+
+    def end_program(self) -> None:
+        pass
+
+def parse_data(data: str) -> list[int]:
+    output_list = []
+    for num_str in data.split(','):
+        if num_str[0] == '-':
+            output_list.append(0 - int(num_str[1:]))
+        else:
+            output_list.append(int(num_str))
+    return output_list
+
+def try_phase_sequence(program: list[int], seq: tuple[int, ...]) -> int:
+    if len(seq) != 5:
+        raise ValueError("Sequence must contain exactly 5 numbers")
+    output_signal = 0
+    for i in range(5):
+        input_signal = output_signal
+        phase_setting = seq[i]
+        comp = Computer(program, [phase_setting, input_signal])
+        comp.execute_program()
+        output_signal = comp.output_list[-1]
+    return output_signal
+        
+def part_one(data: str):
+    program = parse_data(data)
+    max_signal = 0
+    for seq in itertools.permutations([0, 1, 2, 3, 4]):
+        signal = try_phase_sequence(program, seq)
+        max_signal = max(signal, max_signal)
+    return max_signal
+
+def try_phase_sequence_part_two(program: list[int], seq: tuple[int, ...]) -> int:
+    if len(seq) != 5:
+        raise ValueError("Sequence must contain exactly 5 numbers")
+    output_signal = 0
+    for i in range(5):
+        input_signal = output_signal
+        phase_setting = seq[i]
+        comp = Computer(program, [phase_setting, input_signal])
+        comp.execute_program()
+        output_signal = comp.output_list[-1]
+    return output_signal
+
+def part_two(data: str):
+    ...
+    
+def run_tests(tests: list[tuple[str, Any]], fn: Callable):
+    for i, example in enumerate(tests, start=1):
+        data, answer = example
+        test_answer = fn(data)
+        print(f"Test #{i}: {test_answer == answer}",
+              f"({test_answer})")
+
+def main():
+    run_tests(TESTS_PART_ONE, part_one)
+    print(f"Part One (input):  {part_one(INPUT)}")
+    run_tests(TESTS_PART_TWO, part_two)
+    # print(f"Part Two (input):  {part_two(INPUT)}")
+
+    random_tests()
+
+def random_tests():
+    ...
+       
+if __name__ == '__main__':
+    main()
