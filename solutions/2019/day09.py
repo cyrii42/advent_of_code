ee572dc6cadc6f32f733d911f2efb76c6ec924cc
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-04 19:07:08 -0500

    clean up unused imports

--- solutions/2019/day09.py
+++ solutions/2019/day09.py
@@ -1,29 +1,8 @@
-import functools
-import hashlib
-import itertools
-import json
-import math
-import operator
-import os
-import sys
-import re
-from collections import defaultdict, deque
-from copy import deepcopy
-from dataclasses import dataclass, field
-from enum import Enum, IntEnum, StrEnum
 from pathlib import Path
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-from typing import Callable, Generator, NamedTuple, Optional, Self, Any
-
-import numpy as np
-import pandas as pd
-import polars as pl
-import networkx as nx
-from alive_progress import alive_bar, alive_it
 from rich import print
 
 import advent_of_code as aoc
-from intcode import IntCode, IntCodeReturn, IntCodeReturnType, parse_intcode_program
+from intcode import IntCode, IntCodeReturnType, parse_intcode_program
 
 CURRENT_FILE = Path(__file__)
 YEAR = int(CURRENT_FILE.parts[-2])
@@ -34,7 +13,6 @@ EXAMPLES_PART_ONE = [
     '1102,34915192,34915192,7,4,7,99,0',
     '104,1125899906842624,99'
 ]
-TESTS_PART_TWO = []
 INPUT = aoc.get_input(YEAR, DAY)
    
 def part_one(data: str):
@@ -53,13 +31,6 @@ def part_two(data: str):
         if result.type == IntCodeReturnType.HALT:
             return comp.output_queue
 
-def run_tests(tests: list[tuple[str, Any]], fn: Callable):
-    for i, example in enumerate(tests, start=1):
-        data, answer = example
-        test_answer = fn(data)
-        print(f"Test #{i}: {test_answer == answer}",
-              f"({test_answer})")
-
 def main():
     print(f"Part One (example #1):  {part_one(EXAMPLES_PART_ONE[0])}")
     print(f"Part One (example #2):  {part_one(EXAMPLES_PART_ONE[1])}")
@@ -67,11 +38,5 @@ def main():
     print(f"Part One (input):  {part_one(INPUT)}")
     print(f"Part Two (input):  {part_two(INPUT)}")
 
-    random_tests()
-
-def random_tests():
-    ...
-
-       
 if __name__ == '__main__':
     main()
