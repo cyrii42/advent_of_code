71effa2ba7a4d79075d36086a39af4f59a524ceb
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-05 11:10:40 -0500

    removed call to `Puzzle.write_to_db()` from `get_puzzle` (because Puzzle object does it itself now, when it can't find an ID on the database)

--- src/advent_of_code/cli.py
+++ src/advent_of_code/cli.py
@@ -20,7 +20,6 @@ def get_puzzle(year: int, day: int) -> Puzzle:
         return Puzzle.from_database(year, day)
     except PuzzleNotFound:
         puzzle = Puzzle.from_server(year, day)
-        puzzle.write_to_db()
         return puzzle
 
 @app.command(help='Make \"solutions\" subdirectories for 2015 through the latest AOC year (if not already created)')
