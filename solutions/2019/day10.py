892e670e24741de14c7d18f7cc0a94e8b1e19b48
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2025-12-05 17:53:33 -0500

    2019 day 10:  starting over!

--- solutions/2019/day10.py
+++ solutions/2019/day10.py
@@ -108,88 +108,12 @@ def create_graph(node_dict: NodeDict) -> NodeGraph:
         
     return graph
 
-
-
-    
-
-
-def count_asteroids_along_vector(node: Node, direction: Direction, node_dict: NodeDict) -> int:
-    x, y, _ = node
-    dx, dy = DIRECTION_DELTAS[direction]
-
-    total_asteroids = 0
-    print(f"{node.x},{node.y} - {direction.name}")
-    while True:
-        x, y = (x + dx, y + dy)
-        try:
-            node_at_point = node_dict[(x, y)]
-            if node_at_point.node_type == NodeType.ASTEROID:
-                print(f"\t - {node_at_point.node_type}")
-        except KeyError:
-            return total_asteroids
-        else:
-            if node_at_point.node_type == NodeType.ASTEROID:
-                total_asteroids += 1    
-
-def count_detectable_asteroids(node: Node, node_dict: NodeDict) -> int:
-    # for each of the eight directions, count the total number of asteroids along
-    # that vector -- for each vector, any asteroids after the first one are blocked
-
-    total_asteroids = len([node for node in node_dict.values() if node.node_type == NodeType.ASTEROID])
-    total_blocked_asteroids = 0
-    print(f"\n----- {node.x},{node.y} ({total_asteroids} total asteroids) -----")
-    for direction in Direction:
-        total_asteroids_on_vector = count_asteroids_along_vector(node, direction, node_dict)
-        total_blocked_asteroids += max(0, total_asteroids_on_vector - 1)
-    total_detectable_asteroids = total_asteroids - total_blocked_asteroids - 1
-    print(f"{node.x},{node.y}: {total_blocked_asteroids} blocked asteroids")
-    print(f"{node.x},{node.y}: {total_detectable_asteroids} detectable asteroids")
-    return total_detectable_asteroids
-                
-    
 def part_one(data: str):
-    # print(data)
-    # node_dict = create_node_dict(data)
-    # count_asteroids_along_lines_of_sight([n for n in node_dict.values() if n.node_type == NodeType.ASTEROID][0], node_dict)
-
-    # all_asteroid_nodes = [node for node in node_dict.values() if node.node_type == NodeType.ASTEROID]
-    # highest_total = 0
-    # for node in all_asteroid_nodes:
-    #     detectable_asteroids = count_detectable_asteroids(node, node_dict)
-    #     # print(node, detectable_asteroids)
-    #     highest_total = max(highest_total, detectable_asteroids)
-    # return highest_total
-
     node_dict = create_node_dict(data)
     asteroids = [n for n in node_dict.values() if n.node_type == NodeType.ASTEROID]
-    num_asteroids = len(asteroids)
-    print(num_asteroids)
-    blocked_dict = {asteroid: (num_asteroids - count_asteroids_along_lines_of_sight(asteroid, node_dict) - 1) for asteroid in asteroids}
-    print(blocked_dict)
-        
-
-def count_asteroids_along_lines_of_sight(node1: Node, node_dict: NodeDict) -> int:
-    print(node1)
-    other_asteroids = [n for n in node_dict.values() if n != node1 and n.node_type == NodeType.ASTEROID]
-
-    angle_set = set()
-    blocked_asteroids = 0
-    for node2 in other_asteroids:
-        angle = abs(math.atan2(node2.y, node2.x) - math.atan2(node1.y, node1.x))
-        print(angle)
-        if angle in angle_set and node2.node_type == NodeType.ASTEROID:
-            blocked_asteroids += 1
-            print(f"blocked asteroid: {node2}")
-        angle_set.add(angle)
-    return blocked_asteroids
-
 
 def part_two(data: str):
     ...
-
-def find_integer_points_along_line(rise: int, run: int):
-    slope = rise / run
-    
 
 def main():
     print(f"Part One (example):  {part_one(EXAMPLE)}")
@@ -200,20 +124,7 @@ def main():
     # random_tests()
 
 def random_tests():
-    x1, y1 = (3, 4)
-    x2, y2 = (1, 0)
-    # x1, y1 = (3, 10)
-    # x2, y2 = (8.3, 16.5)
-    rise = y2 - y1
-    run = x2 - x1
-    slope = rise / run
-    numerator, denominator = slope.as_integer_ratio()
-
-    # print(math.gcd(-2, -4) + 1)
-    # print()
-    # print(math.tan(slope))
-    print(slope)
-    print(math.atan2(rise, run))
+    ...
            
 if __name__ == '__main__':
     main()
